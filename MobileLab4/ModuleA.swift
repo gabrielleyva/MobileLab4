3044e186e943bbdaa2123c9a7514ea6785f20e50
Gabo <gleyvamerino@smu.edu>
2017-10-10 16:45:03 -0500

    First commit

---
+++ MobileLab4/ModuleA.swift
@@ -0,0 +1,14 @@
+//
+//  ModuleA.swift
+//  MobileLab4
+//
+//  Created by Gabriel I Leyva Merino on 10/10/17.
+//  Copyright © 2017 Leyva_Phadate. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ModuleA: UIViewController {
+    
+}
---
+++ MobileLab4/ModuleB.swift
@@ -0,0 +1,14 @@
+//
+//  ModuleB.swift
+//  MobileLab4
+//
+//  Created by Gabriel I Leyva Merino on 10/10/17.
+//  Copyright © 2017 Leyva_Phadate. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ModuleB: UIViewController {
+    
+}
